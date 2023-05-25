e9694f3009669fc4dd9ead78ed34a57d2637c3c0
hwangblood <hwangblood@gmail.com>
2023-05-25 21:37:17 +0800

    App Intro & Retrofit Setup

--- app/src/main/java/com/hwangblood/android/fakestore/MainActivity.kt
+++ app/src/main/java/com/hwangblood/android/fakestore/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.hwangblood.android.fakestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://fakestoreapi.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        val productsService = retrofit.create(ProductsService::class.java)
+        lifecycleScope.launchWhenStarted {
+            val response = productsService.getAllProduxts()
+            Log.i("DATA",response.body().toString())
+        }
+    }
+
+    interface  ProductsService{
+        @GET("products")
+        suspend fun getAllProduxts(): Response<List<Any>>
     }
 }
